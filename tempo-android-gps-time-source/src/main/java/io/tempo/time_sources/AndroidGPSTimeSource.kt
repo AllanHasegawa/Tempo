fa962178b47c045c5601909c6388116e6f091cac
zhenfang Zhang <fangzhzh@gmail.com>
2020-01-07 10:34:32 +1300

    - follow the gradle version rule

--- tempo-android-gps-time-source/src/main/java/io/tempo/time_sources/AndroidGPSTimeSource.kt
+++ tempo-android-gps-time-source/src/main/java/io/tempo/time_sources/AndroidGPSTimeSource.kt
@@ -23,7 +23,7 @@ import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
-`import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat
 import io.reactivex.BackpressureStrategy
 import io.reactivex.Flowable
 import io.reactivex.Single
